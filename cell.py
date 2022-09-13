716c3e343007801ecaf1263178e676fc578fb836
Leonardo Marazzo <leonardojmarazzo@gmail.com>
2022-09-13 00:48:42 -0300

    Rename block to cell

--- cell.py
+++ cell.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from copy import deepcopy
 from dataclasses import dataclass
 from operator import attrgetter
@@ -11,43 +12,6 @@ class Cell:
     cuadrant: int
     possibilities: Optional[Set[int]]
     number: Optional[int]
-
-
-def get_specific_block(blocks: List[Cell], row, column):
-    for block in blocks:
-        if block.row == row and block.column == column:
-            return block
-        
-    raise Exception(row, column)
-
-
-def get_blocks_from_row(blocks: List[Cell], row):
-    return [block for block in blocks if block.row == row]
-
-
-def get_blocks_from_column(blocks: List[Cell], column):
-    return [block for block in blocks if block.column == column]
-
-
-def get_blocks_from_cuadrant(blocks: List[Cell], cuadrant):
-    return [block for block in blocks if block.cuadrant == cuadrant]
-
-
-def is_different(sudoku: List[Cell], copy_sudoku: List[Cell]):
-    blocks = sorted(sudoku, key=attrgetter("row", "column"))
-    blocks_copy = sorted(copy_sudoku, key=attrgetter("row", "column"))
-    for i in range(len(blocks)):
-        block = blocks[i]
-        block_copy = blocks_copy[i]
-
-        if block_copy.number != block.number:
-            return True
-
-        if block_copy.possibilities != block.possibilities:
-            return True
-
-    return False
-
-
-def copy_sudoku(sudoku: List[Cell]):
-    return [deepcopy(block) for block in sudoku]
+    
+    def is_same_cell(self, cell: Cell):
+        return self.row == cell.row and self.column == cell.column
